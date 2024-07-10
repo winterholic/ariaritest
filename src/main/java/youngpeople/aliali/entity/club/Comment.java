8bdf3ee1012e41ad9b2e4b9656b08f969800a230
대선 <iuholic83@naver.com>
2024-07-10 16:10:30 +0900

    Feat: 게시글 엔티티 수정, enum생성

---
+++ src/main/java/youngpeople/aliali/entity/club/Comment.java
@@ -0,0 +1,47 @@
+package youngpeople.aliali.entity.club;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import youngpeople.aliali.entity.BaseEntity;
+import youngpeople.aliali.entity.member.Member;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@NoArgsConstructor
+@Where(clause = "activated = true")
+@SQLDelete(sql = "UPDATE post SET activated = false WHERE comment_id = ?")
+@Getter
+public class Comment extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String text;
+    private Boolean secret;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    public Comment(Post post, Member member, String text, Boolean secret, Comment parentComment) {
+        this.post = post;
+        this.member = member;
+        this.text = text;
+        this.secret = secret;
+        this.parentComment = parentComment;
+    }
+}
+
---
+++ src/main/java/youngpeople/aliali/entity/enumerated/PostType.java
@@ -0,0 +1,2 @@
+package youngpeople.aliali.entity.enumerated;public class PostType {
+}
