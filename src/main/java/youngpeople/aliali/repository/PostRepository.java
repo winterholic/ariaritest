0787ee307591cad1dc52570eed657be0494db715
대선 <iuholic83@naver.com>
2024-07-11 17:22:27 +0900

    Feat: 커밋 실수한 부분 수정

--- src/main/java/youngpeople/aliali/repository/PostRepository.java
+++ src/main/java/youngpeople/aliali/repository/PostRepository.java
@@ -3,7 +3,12 @@ package youngpeople.aliali.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import youngpeople.aliali.entity.club.Post;
+import youngpeople.aliali.entity.enumerated.PostType;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByClubIdAndPostTypeAndFixed(Long clubId, PostType postType, Pageable pageable, boolean Fixed);
 }
