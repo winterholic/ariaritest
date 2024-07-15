24eb7c93299cb951c429e8ed291aaa3439f0917f
haus <won9619v@naver.com>
2024-07-15 14:28:56 +0900

    Fix: 알림 컨트롤러 수정

--- src/main/java/youngpeople/aliali/controller/BlockController.java
+++ src/main/java/youngpeople/aliali/controller/BlockController.java
@@ -7,12 +7,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import youngpeople.aliali.dto.BasicResDto;
 import youngpeople.aliali.service.BlockService;
 
 @Slf4j
 @RestController
+@RequestMapping("/block")
 @RequiredArgsConstructor
 public class BlockController {
 
